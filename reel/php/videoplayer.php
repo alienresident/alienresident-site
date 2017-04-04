9de0bb0f02b76b46633b98e2d63d89c76d96e8e5
Mark Reilly <mark@alienresident.net>
2017-04-04 09:26:17 -0400

    Adding Video Portfolio
    Adding the video portfolio for the reel

---
+++ reel/php/videoplayer.php
@@ -0,0 +1,129 @@
+<?php include_once('videoplayer_functions.php'); ?>
+<?php include_once('videoplayer_constructor.php'); ?>
+<!doctype html>
+<html>
+  <head>
+  <meta charset="utf-8">
+  <title><?php print $website_title; ?></title>
+<?php if($responsive): ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+<?php endif; ?>
+<?php if($mediaelementplayer): ?>
+  <link rel="stylesheet" href="<?php print $root_dir ?>css/mediaelementplayer.min.css">
+<?php endif; ?>
+<?php if($stylesheet): ?>
+  <link rel="stylesheet" href="<?php print $stylesheet ?>">
+<?php endif; ?>
+<?php if($dropdown): ?>
+  <script>
+    function reload(form) {
+      var val=form.dirurl.options[form.dirurl.options.selectedIndex].value;
+      self.location='<?php print $playerurl ?>?dirurl=' + val;
+    }
+    function reload2(form) {
+      var val=form.dirurl.options[form.dirurl.options.selectedIndex].value;
+      var val2=form.fileurl.options[form.fileurl.options.selectedIndex].value;
+      self.location='<?php print $playerurl ?>?dirurl=' + val + '&fileurl=' + val2;
+    }
+  </script>
+  <?php endif; ?>
+  <?php if($mediaelementplayer): ?>
+  <?php if($offline): ?>
+    <script src="<?php print $root_dir ?>js/jquery-1.9.1.min.js"></script>
+  <?php else: ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <?php endif; ?>
+    <script src="<?php print $root_dir ?>js/mediaelement-and-player.min.js"></script>
+<?php endif; ?>
+</head>
+<body class="<?php print $body_classes; ?>">
+
+<div id="wrapper">
+
+  <div id="topnav">
+    <a name="top"></a>
+    <ul class="mainnav">
+      <li class="first"><a href="../design/" class="mainnav" title="View a portfolio of interactive media design">design</a></li>
+      <li class="inline"><a href="//resume.markreilly.com/" class="mainnav" title="View Mark Reilly's r&#233;sum&#233;">r&#233;sum&#233;</a></li>
+      <li class="inline">reel</li>
+      <li class="inline"><a href="../films/" class="mainnav" title="Watch the films of alienresident.net">films</a></li>
+    </ul>
+    <object type="application/x-shockwave-flash" data="../media/flash/232x20.swf?path=..%2Fmedia%2Fflash%2Falienresident-net-logo.swf" width="232" height="20">
+      <param name="movie" value="../media/flash/232x20.swf?path=..%2Fmedia%2Fflash%2Falienresident-net-logo.swf" />
+      <param name="wmode" value="transparent" />
+      <a href="../" title="Home"><img src="../images/main/alienresident_dot_net.png" alt="alienresident.net logo" /></a>
+    </object>
+  </div> 
+<div id="main">
+  <main role="main">
+    <nav role="navigation">
+<?php if($dropdown): ?>
+      <form action="<?php print $_SERVER['PHP_SELF']; ?>" id="dropdown_menu" method="get">
+        <select id="dirurl" role="listbox" onchange="reload(this.form)">
+          <option value=''>Select a Directory</option>
+          <?php print $select1; ?>
+        </select>
+          <?php print $select2; ?>
+      </form>
+<?php endif; ?>
+<?php if($prev_next): ?>
+      <ul role="menubar">
+        <li role="menuitem"><?php print $prev; ?></li>
+        <li role="menuitem"><?php print $next; ?></li>
+      </ul>
+<?php endif; ?>
+    </nav>
+    <article>
+<?php if($header): ?>
+    <header>
+<?php if($h1): ?>
+      <h1><?php if(isset( $heading1 )) { print $heading1; } ?></h1>
+<?php endif; ?>
+<?php if($h2): ?>
+      <h2><?php if(isset( $heading2 )) { print $heading2; } ?></h2>
+<?php endif; ?>
+    </header>
+<?php endif; ?>
+    <figure class="videoplayer">
+<?php if($html5_video): ?>
+      <?php print $video_tag_open; ?>
+      <?php print $video_tag_sources; ?>
+<?php if($flash_fallback): ?>
+          <!-- Flash fallback for non-HTML5 browsers without JavaScript -->
+        <?php print $flash_object_open; ?>
+<?php endif; ?>
+            <!-- Image as a last resort -->
+            <?php print $video_image_source; ?>
+<?php if($flash_fallback): ?>
+          <?php print $flash_object_close; ?>
+<?php endif; ?>          
+      <?php print $video_tag_close; ?>
+<?php endif; ?>
+<?php if($playlist): ?>
+<aside role="complimentary">
+  <nav role="navigation">
+    <?php print $playlist_list; ?>
+  </nav>
+</aside>
+<?php endif; ?>
+    </figure>
+    <?php if($footer): ?>
+        <footer role="contentinfo">
+          <p><?php print $footer_text; ?></p>
+        </footer>
+    <?php endif; ?>
+  </article>
+  </main>
+ </div> 
+<?php if($mediaelementplayer): ?>
+  <script>
+  $('audio,video').mediaelementplayer({
+    success: function(player, node) {
+       $('#' + node.id + '-mode').html('mode: ' + player.pluginType);
+    }
+  })
+</script>
+<?php endif; ?>
+</div>
+</body>
+</html> 
---
+++ reel/player.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Video Player Config: the Custom Variables You Can Set
+ */
+
+/**
+ * Server Config
+ */
+$dir = 'video';
+$playerurl = 'player.php';
+$root_dir = ''; 
+ 
+/**
+ * Page Elements
+ */
+$websitename = 'Mark Reilly&#039;s Reel';
+$stylesheet = $root_dir . '../style/stylesheet.css';
+$responsive = true;
+$mediaelementplayer = false;
+$dropdown = true;
+$prev_next = false;
+$prev_text = '<span class="hidden">Previous</span>';
+$next_text = '<span class="hidden">Next</span>';
+$playlist = false;
+$playlist_descriptions = true;
+
+/**
+ * Movie Dimensions
+ */
+$w = 640;
+$h = 480;
+
+/**
+ * Video Parameters
+ */
+$html5_video = true;
+$poster_image = true;
+$video_tag_attributes = 'controls autoplay';
+$flash_fallback = false;
+
+/**
+ * Content
+ */
+$header = false;
+$h1 = false;
+$h2 = true;
+//$heading2 = '<a href="'. $root_dir .  $playerurl .'" title="Home" rel="home">Mark Reilly&#039;s Reel</a>';
+$heading2 = '';
+$footer = true;
+$footer_text = '';
+
+/**
+ * Prettify Filenames and Add Playlist Descriptions
+ */
+$names_file = $root_dir . 'names.txt';
+$playlist_descriptions_file = $root_dir . 'descriptions.txt';
+
+/**
+ * Advanced Player Configurable Variables
+ */
+ 
+$offline = false; // for development only: use a local copy of jQuery rather than the Google CDN
+   
+/**
+ * Filename Variables
+ */
+$dir_offset_start = 3; // remove the first 'n' characters from the directory name i.e. 3 = '01_' from O1_Artwork
+$file_offset_start = 0; // remove the first 'n' characters from the filename
+$file_offset_end = -3; // remove the last 'n' characters before file extension i.e. -3 will remove '-sd' from filename-sd.mp4
+
+/**
+ * CSS Body Classes
+ */
+$body_classes = "player";
+
+/**
+ * Video sources, codecs, and media queries.
+ *
+ * What codecs and mediaquries are to be used with certain filenames: i.e '-sd' = (Standard Definition), 'avc1.64001E, mp4a.40.2' h264 high profile, aac audio, 'all and (max-width: 854px). Media Queries should only be used if $responsive = true;
+ */
+$video_sources = array();
+//$video_sources[] = array('filename_stem' => '-hd', 'codec' =>'avc1.64001E, mp4a.40.2', 'media' =>'all and (min-width: 1280px)');
+$video_sources[] = array('filename_stem' => '-sd', 'codec' =>'avc1.64001E, mp4a.40.2', 'media' =>'all and (max-width: 800px)');
+//$video_sources[] = array('filename_stem' => '-sm', 'codec' =>'avc1.42E01E, mp4a.40.2', 'media' =>'all and (max-width: 512px)'); 
+//$video_sources[] = array('filename_stem' => '-wb', 'codec' =>'vp8, vorbis', 'media' =>'');
+
+/**
+ * Which h264 File to Use a Flash Fallback
+ */
+$flash_fallback_source = '-sd';
+
+
+
+/**
+ * Rest of the Php Part, Do Not Edit
+ */
+include_once($root_dir . 'php/videoplayer.php');
+?>
